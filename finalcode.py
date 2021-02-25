d915dfaae773a9b3755bfe3765e7194c58a22dd2
99003736 <ajit.bijapur@ltts.com>
2021-02-25 18:31:45 +0530

    changes in loop

--- finalcode.py
+++ finalcode.py
@@ -15,20 +15,20 @@ class find(text_processing):
             user_search_input=input("enter the words: ")
             lines=0
             #pattern=re.compile(user_search_input,re.I)
-        with open ("input_file.txt","rt")as file_input:
-            cc=file_input.read()
-            file_input1=re.split('\W+',cc)
-            for file_line in range(len(file_input1)):
-                lines+=1
-                if re.fullmatch(user_search_input,file_input1[file_line],re.M |re.I):
-                    count+=1
-                    with open("{}.txt".format(user_search_input),'a') as file_output:
-                        file_output.writelines(str(count)+':')
-                        file_output.write(file_input1[file_line-1]+" " +file_input1[file_line]+" "+file_input1[file_line+1]+'\n') 
+            with open ("input_file.txt","rt")as file_input:
+                cc=file_input.read()
+                file_input1=re.split('\W+',cc)
+                for file_line in range(len(file_input1)):
+                    lines+=1
+                    if re.fullmatch(user_search_input,file_input1[file_line],re.M |re.I):
+                        count+=1
+                        with open("{}.txt".format(user_search_input),'a') as file_output:
+                            file_output.writelines(str(count)+':')
+                            file_output.write(file_input1[file_line-1]+" " +file_input1[file_line]+" "+file_input1[file_line+1]+'\n') 
 
-       
-            print(count)
-           
+        
+                print(count)
+            
         
 filename = 'input_file.txt'
 
